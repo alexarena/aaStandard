580db0f682844462e8b689ec2460262810787c88
Alex Arena <im@alexarena.com>
2019-02-01 18:58:25 -0800

    new approach for this package

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 module.exports = {
-  promisify: require('./promisify')
-  object: require('./object')
+  pick: ({properties,from}) => {
+      const returnObj = {}
+      for(const prop of properties){
+        returnObj[prop] = from[prop]
+      }
+      return returnObj
+  },
+  useEnv: ({suppressLog})=>{
+    try{
+      const env = require('./env')
+      console.log('Registering environment variables:')
+      for(const [key,value] of Object.entries(env)){
+        process.env[key] = value
+        console.log(`-${key}`)
+      }
+    }
+    catch(e){
+      console.log('Warning - no .env file found.')
+    }
+  }
 }
--- object/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  getNestedProperty: require('./getNestedProperty')
-}
